36e87909ae9bea9a0e7b4459fee1aa95a7ef3d6d
matiasvidal9 <matias98vidal@hotmail.com>
2025-12-21 16:15:58 -0300

    se creo el navbar agregando estilos css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./css/NavBar.css";
+import Navbar from "./components/Navbar.jsx";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <h1>LOOk</h1>
+            </div>
+            <ul className="navbar-links">
+                <li><a href="#">Men</a></li>
+                <li><a href="#">Women</a></li>
+                <li><a href="#">Accessories</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
