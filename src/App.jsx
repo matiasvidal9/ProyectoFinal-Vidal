dd598466d4b2d0528e523beb31e3576ff5cffe85
matiasvidal9 <matias98vidal@hotmail.com>
2025-12-21 17:17:08 -0300

    se agrego el ItemListContainer y se termino de diseñar parte del hero con una imagen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import "./css/NavBar.css";
 import "./css/CartWidget.css";
-import Navbar from "./components/Navbar.jsx";
-import './App.css'
-import CartWidget from "./components/CartWidget.jsx";
+import"./css/ItemListContainer.css";
+import "./css/Hero.css";
+import NavBar from "./components/Navbar";
+import ItemListContainer from "./components/ItemListContainer";
+import heroImg from "./assets/heroImg.png"; 
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavBar />
+      
+      {/* Sección Hero inspirada en Marion */}
+      <div className="hero-section">
+        <img src={heroImg} alt="LOOk Fashion Collection" className="hero-image" />
+        <div className="hero-overlay">
+          <ItemListContainer greeting="New Arrivals: Season 2025" />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = ({ greeting }) => {
+    return (
+        <div className="item-list-container">
+        <h2 className="greeting-text">{greeting}</h2>
+        </div>
+    );
+};
+
+export default ItemListContainer;
