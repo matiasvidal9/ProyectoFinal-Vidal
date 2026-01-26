a8c2e2f9538170aa0d6100801cf8d1ea06627918
matiasvidal9 <matias98vidal@hotmail.com>
2026-01-25 23:27:42 -0300

    Se aplicó el paradigma del Modelo Cliente-Servidor para gestionar el intercambio de información bajo demanda del cliente, implementación de la función 'getProductById' para simular una petición de tipo GET a un recurso específico del servidor, se estructuró la obtención de datos para permitir el acceso a recursos mediante un ID específico integrado en la ruta, implementación de objetos Promise y manejo de estados asíncronos y uso de retardos controlados (setTimeout) y el procesamiento de datos en formato JSON

--- src/mock/mockData.js
+++ src/mock/mockData.js
@@ -1,7 +1,7 @@
 const products = [
-    { id: 1, name: "Buzo Oversize", price: 25000, category: "men", stock: 10, img: "../assets/men/Buzo1-men.png" },
-    { id: 2, name: "Remera Oversize", price: 45000, category: "men", stock: 5, img: "../assets/men/Remera1-men.png" },
-    { id: 3, name: "Jean Recto", price: 12000, category: "men", stock: 15, img: "../assets/men/Jean1-men.png" },
+    { id: 1, name: "Buzo Oversize", price: 25000, category: "men", stock: 10, img: "../public/men/Buzo1-men.png" },
+    { id: 2, name: "Remera Oversize", price: 45000, category: "men", stock: 5, img: "../public/men/Remera1-men.png" },
+    { id: 3, name: "Jean Recto", price: 12000, category: "men", stock: 15, img: "../public/men/Jean1-men.png" },
 ];
 
 export const getProducts = () => {
@@ -9,5 +9,15 @@ export const getProducts = () => {
         setTimeout(() => {
             resolve(products);
         }, 1500); 
+    });
+};
+
+export const getProductById = (productId) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            // Buscamos el producto que coincida con el ID solicitado
+            const product = products.find(prod => prod.id === parseInt(productId));
+            resolve(product);
+        }, 1500);
     });
 };
